370f1ce0a75af8f208faa1f9e25348cf767d60a6
Иван Хомичков <ivan.khomichkov@gmail.com>
2017-08-19 17:42:39 +0300

    First init

--- ConsecStrings/Program.cs
+++ ConsecStrings/Program.cs
@@ -10,6 +10,32 @@ namespace ConsecStrings
     {
         static void Main(string[] args)
         {
+            string[] temp = { "zone", "abigail", "theta", "form", "libe", "zas" };
+            Console.WriteLine(LongestConsec(temp,2));
+
         }
+        public static String LongestConsec(string[] strarr, int k)
+        {
+            List<string> ConsecStrings = new List<string>();
+            int i = 0;
+            do
+            {
+                string temp = "";
+                for (int j = i; j < j + k; j++)
+                {
+                    temp += strarr[j];
+                }
+                ConsecStrings.Add(temp);
+                i++;
+            } while (i + k < strarr.Length);
+
+            int maxLength = ConsecStrings[0].Count;
+            int indexOfString = 0;
+
+            return "";
+            
+                
+        // your code
+    }
     }
 }
