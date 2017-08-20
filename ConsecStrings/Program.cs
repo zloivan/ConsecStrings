b555ca83d20a13adea3eb6dab9b9649a8fb557f7
Иван Хомичков <ivan.khomichkov@gmail.com>
2017-08-20 22:32:50 +0300

    Finikshed fist solution in CodeWars

--- ConsecStrings/Program.cs
+++ ConsecStrings/Program.cs
@@ -11,31 +11,53 @@ namespace ConsecStrings
         static void Main(string[] args)
         {
             string[] temp = { "zone", "abigail", "theta", "form", "libe", "zas" };
-            Console.WriteLine(LongestConsec(temp,2));
+            
+            Console.WriteLine(LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3));
+            Console.WriteLine($"LC with LIQN: {LongestConsecWithLinq(temp, 1)}");
 
         }
         public static String LongestConsec(string[] strarr, int k)
         {
-            List<string> ConsecStrings = new List<string>();
-            int i = 0;
-            do
-            {
-                string temp = "";
-                for (int j = i; j < j + k; j++)
+            List<string> AllConsecStrings = new List<string>();
+
+            if (k > strarr.Length || strarr.Length==0|| k<=0)
+                return "";
+                for (int i = 0; i < strarr.Length+1-k; i++)
                 {
-                    temp += strarr[j];
+                    string BufferString = "";
+                    for (int j = i; j < i + k; j++)
+                    {
+                        BufferString += strarr[j];
+                    }
+                    AllConsecStrings.Add(BufferString);
                 }
-                ConsecStrings.Add(temp);
-                i++;
-            } while (i + k < strarr.Length);
+                
+               
+            
 
-            int maxLength = ConsecStrings[0].Count;
+            
+            int maxLength = AllConsecStrings[0].Length;
             int indexOfString = 0;
-
-            return "";
+            for (int i = 0; i < AllConsecStrings.Count; i++)
+            {
+                if (AllConsecStrings[i].Length > maxLength)
+                {
+                    maxLength = AllConsecStrings[i].Length;
+                    indexOfString = i;
+                }
+            }
+            return AllConsecStrings[indexOfString];
             
                 
-        // your code
+        
     }
+        public static String LongestConsecWithLinq(string[] s, int k)
+        {
+            return s.Length == 0 || s.Length < k || k <= 0 ? ""
+             : Enumerable.Range(0, s.Length - k + 1)
+                         .Select(x => string.Join("", s.Skip(x).Take(k)))
+                         .OrderByDescending(x => x.Length)
+                         .First();
+        }
     }
 }
